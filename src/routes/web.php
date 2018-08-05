b1c30584c67be6422dc47a84f75a79343a07d263
Robson P Oliveira <rpopuc@gmail.com>
2018-08-05 01:08:20 -0300

    Adiciona configuração da base de dados no primeiro acesso ao sistema

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,18 +12,39 @@
 */
 
 Route::get('/', function () {
+    if (env('APP_ENV') == 'local') {
+        if (!\Schema::hasTable('migrations')) {
+            $firstRun = true;
+            Artisan::call('migrate', ['--seed' => true]);
+        }
+        Artisan::call('migrate');
+    }
+
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/events', function() {
-    return view( 'example.events' );
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::any('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::get('/events', function () {
+    return view('example.events');
 });
 
-Route::get('/event', function() {
+Route::get('/event', function () {
     return [
         [
             'type' => 'Evento',
